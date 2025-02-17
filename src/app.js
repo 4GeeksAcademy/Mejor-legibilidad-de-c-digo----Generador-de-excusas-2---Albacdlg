d9b242898b76cda8f4c94ce276a45de2ed420cf3
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-02-17 20:40:43 +0000

    CSS actualizado

--- src/app.js
+++ src/app.js
@@ -7,10 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const who = ["Mi perro", "El profesor", "Un pájaro", "Un alien"];
+  const action = ["comió", "rompió", "perdió", "se llevó"];
+  const what = ["mi tarea", "el proyecto", "mis apuntes", "mi libro"];
+  const when = ["ayer", "esta mañana", "hace un rato", "el lunes pasado"];
 
-  let who = ["Mi perro", "El profesor", "Un pájaro", "Un alien"];
-  let action = ["comió", "rompió", "perdió", "se llevó"];
-  let what = ["mi tarea", "el proyecto", "mis apuntes", "mi libro"];
-  let when = ["ayer", "esta mañana", "hace un rato", "el lunes pasado"];
+  function generateExcuse() {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
 
-  
+  document
+    .getElementById("generateButton")
+    .addEventListener("click", function() {
+      document.querySelector(".excuse").textContent = generateExcuse();
+    });
+
+  document.querySelector(".excuse").textContent = generateExcuse();
+};
